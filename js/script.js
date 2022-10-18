b1f0de6b05acee69ab65095aefb710b4f9ef0315
Andy <kirizleev.andrei@yandex.ru>
2022-10-19 00:27:41 +0500

    added slider and database

--- js/script.js
+++ js/script.js
@@ -191,32 +191,21 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        7,
-        '.menu .container'
-    ).render();
+    const getResources = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        9,
-        '.menu .container'
-    ).render();
+        return await res.json();
+    };
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        8,
-        '.menu .container'
-    ).render();
+    getResources('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            })
+        })
 
     // Forms to server
 
@@ -229,10 +218,22 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     forms.forEach(item => {
-        postData(item);
-    })
+        bindPostData(item);
+    });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -246,27 +247,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function (value, key){
-                object[key] = value;
-            })
+            const json = JSON.stringify(Object.fromEntries(formData.entries()))
 
-
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => data.text())
-              .then(data => {
-                console.log(data);
-                showThanksModal(message.success);
-                statusMessage.remove();
+            postData('http://localhost:3000/requests', json)
+            .then(data => {
+              console.log(data);
+              showThanksModal(message.success);
+              statusMessage.remove();
             }).catch(() => {
-                showThanksModal(message.failure);
+              showThanksModal(message.failure);
             }).finally(() => {
-                form.reset();
+             form.reset();
             })
         })
     }
@@ -292,4 +283,56 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }, 4000)
     }
+
+    //SLIDER
+
+    const sliderItems = document.querySelectorAll('.offer__slide'),
+          sliderPrev = document.querySelector('.offer__slider-prev'),
+          sliderNext = document.querySelector('.offer__slider-next'),
+          currentSlide = document.querySelector('#current');
+
+    function hideSliderItems(){
+        sliderItems.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        })
+    }
+
+    let slideNum = 0;
+
+    function showSliderItem(slideNum){
+        sliderItems[slideNum].classList.add('show', 'fade');
+        sliderItems[slideNum].classList.remove('hide');
+        let a = currentSlide.innerHTML = slideNum + 1;
+        if (a < 10){
+            currentSlide.innerHTML = `0${slideNum + 1}`
+        }
+    }
+
+    hideSliderItems();
+    showSliderItem(slideNum);
+
+    sliderPrev.addEventListener('click', () => {
+        if (slideNum === 0 ){
+            slideNum = sliderItems.length - 1;
+            hideSliderItems();
+            showSliderItem(slideNum);
+        }else{
+            slideNum--;
+            hideSliderItems();
+            showSliderItem(slideNum);
+        }
+    })
+
+    sliderNext.addEventListener('click', () => {
+        if(slideNum === sliderItems.length - 1){
+            slideNum = 0;
+            hideSliderItems();
+            showSliderItem(slideNum);
+        }else{
+            slideNum++;
+            hideSliderItems();
+            showSliderItem(slideNum);
+        }})
+
 })
